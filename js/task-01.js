8214ed701f06ad956368d280d70385f94a1f9e84
WKotlinski <132941585+WKotlinski@users.noreply.github.com>
2023-11-05 20:22:46 +0100

    final changes

--- js/task-01.js
+++ js/task-01.js
@@ -1,19 +1,3 @@
-// Policzy i wypisze w konsoli liczbę kategorii w ul#categories, czyli elementy li.item.
-// Dla każdego elementu li.item na liście ul#categories, znajdzie i wypisze w konsoli tekst
-// nagłówka elementu (tag <h2>) i liczbę elementów w danej kategorii (wszystkich w jej obrębie <li>).
-
-// Po wykonaniu w konsoli znajdą się takie komunikaty.
-
-// Number of categories: 3
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
 const allLi = document.querySelectorAll(".item");
 console.log(`Number of categories:${allLi.length}`);
 allLi.forEach((item) => {
--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const input = document.getElementById("validation-input");
 const checkFunction = function () {
   const inputValue = input.value;
   const neededLength = input.getAttribute("data-length");
-  if ((inputValue.length = neededLength)) {
+  if (inputValue.length == neededLength) {
     input.classList.remove("invalid");
     input.classList.add("valid");
   } else {
